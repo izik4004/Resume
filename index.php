4926f9358f24375cb6855b379f41632c216f9291
unknown <izik4004>
2021-08-19 23:50:29 +0100

    changes to form

--- index.php
+++ index.php
@@ -1,31 +1,3 @@
-<?php 
-
-    if(isset($_POST['btn-send']))
-    {
-       $UserName = $_POST['UName'];
-       $Email = $_POST['Email'];
-       $Subject = $_POST['Subject'];
-       $Msg = $_POST['msg'];
-
-       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
-       {
-           header('index.php?error');
-       }
-       else
-       {
-           $to = "imazik4004@gmail.com";
-
-           if(mail($to,$Subject,$Msg,$Email))
-           {
-               header("index.php?success");
-           }
-       }
-    }
-    else
-    {
-        header("index.php");
-    }
-?>
 
 <html lang="en">
 <head>
