6d14ebf822e092d31bafb8cf184353f6edcd1b0f
unknown <izik4004>
2021-08-20 09:19:45 +0100

    changes to form

--- message.php
+++ message.php
@@ -9,7 +9,7 @@
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "izik4004@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
